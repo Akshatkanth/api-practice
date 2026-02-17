3730d8d67d3b7ba9b7747e2e5076b5077639c428
Akshat <110756105+Akshatkanth@users.noreply.github.com>
2026-02-17 21:01:02 +0530

    feat: add drizzle-orm and pg dependencies, and implement PostgreSQL connection

---
+++ drizzle/db.js
@@ -0,0 +1,19 @@
+const { drizzle } = require("drizzle-orm/node-postgres");
+const { Client } = require("pg");
+require("dotenv").config()
+const client = new Client({
+    host:"localhost",
+    port:5432,
+    user: "postgres",
+    password: process.env.db_password,
+    database: "drizzle_db"
+});
+
+async function connectDB(){
+    await client.connect();
+    console.log("PostgreSQL connected");
+}
+
+const db = drizzle(client);
+
+module.exports = { db, connectDB };
