9b6efd750eaf9e5a5559559e19aa4776f43e1766
Akshat <110756105+Akshatkanth@users.noreply.github.com>
2026-02-19 20:57:56 +0530

    hashing algos practice

---
+++ hashing algos/md5.js
@@ -0,0 +1,16 @@
+const crypto = require("crypto")
+
+function hashWithMD5(input){
+    const hash = crypto
+        .createHash("md5")
+        .update(input)
+        .digest("hex");
+
+    return hash;
+}
+
+const password = "mySecretPassword"
+const hashedPassword = hashWithMD5(password)
+
+console.log("Orignial:", password)
+console.log("MD% Hashed password:", hashedPassword)
