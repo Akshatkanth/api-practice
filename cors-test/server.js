659dc2f65952e83d4b200b0acda8375b7c6f9bbb
Akshat Prakash Kanth <110756105+Akshatkanth@users.noreply.github.com>
2026-03-06 21:03:30 +0530

    cors

--- cors-test/server.js
+++ cors-test/server.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors({
+    origin:"http://localhost:5173"
+}))
+
 app.get("/api/test", (req, res)=>{
-    res.json({message:"Hello from backend"})
+    res.json({message:"Get request works"})
 });
+
+app.post("/api/test", (req, res)=>{
+    res.json({message:"Post request works", body:req.body})
+});
+
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
