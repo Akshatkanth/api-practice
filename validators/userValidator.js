de9f004f608aaffd99c817482fc4598d10591c3d
Akshat Prakash Kanth <110756105+Akshatkanth@users.noreply.github.com>
2026-03-08 23:33:16 +0530

    feat: add zod for validation and create signup schema

---
+++ validators/userValidator.js
@@ -0,0 +1,9 @@
+const { z } = require("zod")
+
+const signupSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+    name: z.string().min(2)
+});
+
+module.exports = signupSchema;
